8516978656bb6614783a3cccf42d27118ce3907d
bingxinjiang03 <bingxinjiang03@gmail.com>
2024-10-07 15:13:22 +0800

    code to generate cot

--- generate.py
+++ generate.py
@@ -2,11 +2,13 @@ import json
 import openai
 openai.api_base = "https://yeysai.com/v1"
 openai.api_key="sk-TmqNTUwMzp2RXHmG3d44868eD27247E4A0771bA203D8922d"
+
+
 def chatgpt(messages):
         try:
             response = openai.ChatCompletion.create(
           #  model='gpt-3.5-turbo-1106',
-            model='gpt-4-1106-preview',
+            model='gpt-4o',
             messages=messages,
             temperature=0.1,
             )
@@ -55,27 +57,103 @@ def search_and_update(json_obj, outlines,target_key, new_value):
 
 def split(obj,max_length):
     titles=list(obj.keys())
-    for key,value in dict.items():
+    for key,value in obj.items():
         if isinstance(value, dict):
             split(obj[key],max_length)
         if isinstance(value, str):
             if(len(value)>max_length):
+                print(2)
                 if(key=="others"):
-                    
+                    split_prompt=f"""You will receive a lot of texts related to diabete. Each text begins with a number in brackets as its serial number. Your task is to classify these texts and come up with a separate title for each class. Please output a title and the serial number of the corresponding texts on each line. The title and serial number are separated by '//', and multiple serial numbers are separated by ','. \nSample output: Output value growth//2,3,5\nGreen agriculture//0,1\nNote: 1. The output must comply with the format requirements and wrap after the serial number set of each title; 2. You can divide the texts into NO MORE THAN 4 classes. \nBelow are multiple texts: \n{value}\nPlease answer the classification results directly in the required format without any extra words."""
+                    messages=[{"role":"user","content":split_prompt}]
+                    gpt_answer=chatgpt(messages)
+                   # print(gpt_answer)
+                    classes=gpt_answer.replace("\n\n","\n").split("\n")
+                    if(len(classes)==1):
+                        return 
+                    articles=value.split("\n")
+                    for new_class in classes:
+                        parts=new_class.split("//")
+                        new_title=parts[0]
+                        numbers=parts[1].split(",")
+                        new_content=""
+                        for number in numbers:
+                            prefix="["+number+"]"
+                            for article in articles:
+                                if article.startswith(prefix):
+                                    new_content+=article
+                                    new_content+="\n"
+                        obj[new_title]=new_content
+                    obj["others"]=""
+                    break
                 else:
-                    obj[key]=split_new(value,key)
+                    split_prompt=f"""You will receive a lot of texts related to {key}. Each text begins with a number in brackets as its serial number. Your task is to classify these texts and come up with a separate title for each class. Please output a title and the serial number of the corresponding texts on each line. The title and serial number are separated by '//', and multiple serial numbers are separated by ','. \nSample output: Output value growth//2,3,5\nGreen agriculture//0,1\nNote: 1. The output must comply with the format requirements and wrap after the serial number set of each title; 2. You have to divide the text into more than 1 classes. 3. You can divide the texts into NO MORE THAN 4 classes. \nBelow are multiple texts: \n{value}\nPlease answer the classification results directly in the required format without any extra words."""
+                    messages=[{"role":"user","content":split_prompt}]
+                    gpt_answer=chatgpt(messages)
+                    classes=gpt_answer.replace("\n\n","\n").split("\n")
+                    if (len(classes)==1):
+                        return 
+                    new_dict={}
+                    articles=value.split("\n")
+                    print(classes)
+                    for new_class in classes:
+                    
+                        parts=new_class.split("//")
+                        new_title=parts[0]
+                        numbers=parts[1].split(",")
+                        new_content=""
+                        for number in numbers:
+                            prefix="["+number+"]"
+                            for article in articles:
+                                if article.startswith(prefix):
+                                    new_content+=article
+                                    new_content+="\n"
+                        new_dict[new_title]=new_content
+                    obj[key]=new_dict
 
-def split_new(value,original_title):
-    split_prompt="You will receive a lot of texts related to {original_title}. Each text begins with a number in brackets as its serial number. Your task is to classify these texts and come up with a separate title for each class. Please output a title and the serial number of the corresponding texts on each line. The title and serial number are separated by '//', and multiple serial numbers are separated by ','. \nSample output: Output value growth //2,3,5\nGreen agriculture //0,1\nNote: 1. The titles should be short and there is no need to repeat fields related to common topics such as {original_title}; 2. The output must comply with the format requirements and wrap after the serial number set of each title; 3. You can divide the texts into NO MORE THAN 4 classes. \nBelow are multiple texts: \n{value}\nPlease answer the classification results directly in the required format without any extra words."
+
 
 
 def merge(obj,max_num):
-    )
+    if isinstance(obj, str):
+            return
+    titles=list(obj.keys())
+   # print(titles)
+    if "others" in titles:
+        titles.remove("others")
+    for key,value in obj.items():
+        if isinstance(value, dict):
+            merge(obj[key],max_num)
+    
+    if(len(titles)>max_num):
+        number_list = [f"[{i}]{string}" for i, string in enumerate(titles)]
+        merge_prompt=f"""You will receive a lot of sub-headings related to {key}. Each sub_heading begins with a number in brackets as its serial number. Your task is to classify these sub-headings and come up with a separate heading for each class. Please output a heading and the serial number of the corresponding sub-headings on each line. The title and serial number are separated by '//', and multiple serial numbers are separated by ','. \nSample output: Output value growth//2,3,5\nGreen agriculture//0,1\nNote: 1. The output must comply with the format requirements and wrap after the serial number set of each title; 2. You can divide the sub-headings into NO MORE THAN 4 classes. \nBelow are multiple sub_headings: \n{number_list}\nPlease answer the classification results directly in the required format without any extra words."""
+        messages=[{"role":"user","content":merge_prompt}]
+        gpt_answer=chatgpt(messages)
+        new_titles=gpt_answer.replace("\n\n","\n").split("\n")
+        for title in new_titles:
+            new_dict={}
+            parts=title.split("//")
+            new_title=parts[0]
+            numbers=parts[1].split(",")
+            if len(numbers)==1:
+                continue
+            else:
+                for number in numbers:
+                    num=int(number)
+                    old_title=titles[num]
+                    new_dict[old_title]=obj[old_title]
+                    del obj[old_title]
+                obj[new_title]=new_dict
+    
 
+            
 def search(obj,text,num,depth=0):
     titles=list(obj.keys())
+    if isinstance(obj, str):
+        return
     if isinstance(obj, dict):
-        if(depth=0):
+        if(depth==0):
             select_title_prompt=f"""You will receive a section heading library and a text for diabetes trials. Please follow the steps below: 1. Understand the text and find the heading that match the text from the section heading library, with no more than 2 best match headings. 2. Summarize the content from the text that is related to each heading. 3. Each line of output must contain two parts: 'subsection heading&&Summary content'. 4. If there is no any heading that matches the text, output 'None'. \n Sample output: Symptoms of COVID-19&&A small number of patients with COVID-19 may experience severe headaches. \n Oral medication&&The trial found that the combination of Nirmatrelvir and Ritonavir 2 was significantly better than using them separately in relieving symptoms such as breathing difficulties and severe headaches caused by COVID-19. \nNote: 1. Output must be in the required format, and only wrap after the summary content; 2. Select headings that are most related to the text, otherwise output 'None' when there is no any related heading; 3. Do not select more than 2 headings. \nHeading library: {titles}\nText: {text}\n\nPlease answer the matched headings directly, one per line, without extra words.""" 
         else:
             select_title_prompt=f"""You will receive a section heading library and a text for diabetes trials. Please follow the steps below: 1. Understand the text and find the heading that match the text from the section heading library. 2. Summarize the content from the text that is related to the heading. 3. The output must contain two parts: 'subsection heading&&Summary content'. 4. If there is no any heading that matches the text, output 'None'. \n Sample output: \n Oral medication&&The trial found that the combination of Nirmatrelvir and Ritonavir 2 was significantly better than using them separately in relieving symptoms such as breathing difficulties and severe headaches caused by COVID-19. \nNote: 1. Output must be in the required format; 2. Select headings that are most related to the text, otherwise output 'None' when there is no any related heading; \nHeading library: {titles}\nText: {text}\n\nPlease answer the matched headings directly without extra words."""
@@ -84,53 +162,74 @@ def search(obj,text,num,depth=0):
        # title="None"
         ##生成新的标题
         if(title=="None"or "None" in title):
-            summary_prompt="Please generate a summary based on the text provided. \nText: {text}\n\nPlease directly respond with your summary without any redundant words."
+            summary_prompt=f"""Please generate a summary based on the text provided. \nText: {text}\n\nPlease directly respond with your summary without any redundant words."""
             messages=[{"role":"user","content":summary_prompt}]
             summary=chatgpt(messages)
-            summary="["+num+"]"+summary+"\n"
+            summary="["+str(num)+"]"+summary+"\n"
             if("others" in titles):
                 obj["others"]+=summary
             else:
                 obj["others"]=summary
         else:
             title=title.replace("\n\n","\n").split("\n")
+            count=0
             for i in range(len(title)):
+                if(count>1):
+                    return
+                count+=1
                 line=title[i].split("&&")
                 name=line[0]
-                summary="["+num+"]"+line[1]+"\n"
+                summary="["+str(num)+"]"+line[1]+"\n"
                 obj_=obj[name]
+                    
                 if isinstance(obj_, dict):
                     search(obj[name],text,num,depth+1)
                 if isinstance(obj_, str):
                     obj[name]+=summary
 
                 
+#def reorganize(obj):
+        
 
+
+
+with open('output6/merge_300.json','r',encoding='utf-8') as g :
+    directory=json.load(g)
 
 def generate_wiki(directory, articles):
-    for i in range(0,401):
+    for i in range(301,401):
         article=articles[i]
         text=article["content"]
         num=article["id"]
+    
         try:
-            seach(directory,text,num)
+            search(directory,text,num,0)
         except Exception as exc:
             error_message = f"Error: {str(exc)}"
+            print(num)
             print(error_message)
             continue
+            
+
         if(article["id"]%10==0):
             name=f"""output6/step_{article["id"]}.json"""
             with open(name, 'w') as file:
                 json.dump(directory, file, indent=4)
-
-            split(directory,3000)
+    
+            split(directory,8000)
             name1=f"""output6/split_{article["id"]}.json"""
             with open(name1, 'w') as file:
                 json.dump(directory, file, indent=4)
 
-            directory=merge(directory,4)
+            merge(directory,10)
             name2=f"""output6/merge_{article["id"]}.json"""
             with open(name2, 'w') as file:
                 json.dump(directory, file, indent=4)
 
+       # if(article["id"]%50==0):
+        #    reorganize(directory)
+         #   name3=f"""output6/reorganize_{article["id"]}.json"""
+          #  with open(name3, 'w') as file:
+           #     json.dump(directory, file, indent=4)
+            
 generate_wiki(directory, articles)
---
+++ generate_cot.py
@@ -0,0 +1,68 @@
+import json
+import openai
+openai.api_base = "https://yeysai.com/v1"
+openai.api_key="sk-TmqNTUwMzp2RXHmG3d44868eD27247E4A0771bA203D8922d"
+def chatgpt(messages):
+        try:
+            response = openai.ChatCompletion.create(
+          #  model='gpt-3.5-turbo-1106',
+            model='gpt-4o',
+            messages=messages,
+            temperature=0.1,
+            )
+            answer = response.choices[0].message["content"]
+            return answer
+        except Exception as exc:
+            error_message = f"Error: {str(exc)}"
+            return error_message
+
+
+
+def read_file_all_content(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+    return content
+
+with open('diabetes_content.json', 'r', encoding='utf-8') as file:
+    articles = [json.loads(line) for line in file]
+
+
+count=0
+with open("diabete_cot.json", "a") as file:
+    for i in range(514,1000):
+        content=articles[i]["content"]
+        skip_prompt=f"""Given an abstract of a paper, determine whether the abstract contains background knowledge or an introduction. If so, output YES; if not, output NO. 
+    Here is the abstract:{content}"""
+        messages=[{"role":"user","content":skip_prompt}]
+        skip_content=chatgpt(messages)
+        if(skip_content=="NO"):
+            count+=1
+            continue
+        filter_prompt=f"""Given an abstract of a paper, delete the detailed experimental methods and only keep the Introduction, background knowledge, Objective
+##example:
+abstract:
+[130] Type 2 diabetes and dyslipidemia are diseases that collectively increase the risk of patients developing cardiovascular complications. Several incretin-based drugs are reported to improve lipid metabolism, and one of these medications, anagliptin, is a dipeptidyl peptidase-4 (DPP-4) inhibitor that has been shown to decrease serum triglyceride and low-density lipoproteins cholesterol. This study aimed to conduct an investigation into the effects of anagliptin on serum lipid profiles. This multicenter, open-label, randomized (1:1), parallel group study was designed to evaluate the effects of anagliptin on serum lipid profiles (triglycerides, lipoproteins, apolipoproteins, and cholesterol fractions). The study involved 24 patients with type 2 diabetes at two participating hospitals for a period of 24 weeks. Patients were randomly assigned to the anagliptin (n = 12) or control (n = 12) groups. Patients in the anagliptin group were treated with 200 mg of the drug twice daily. Patients in the control group did not receive anagliptin, but continued with their previous treatment schedules. Lipid metabolism was examined under fasting conditions at baseline and 24 weeks. Patients treated with anagliptin for 24 weeks exhibited significantly reduced levels of serum apolipoprotein B-48, a marker for lipid transport from the intestine, compared with the control group patients (P < 0.05). After 24 weeks of treatment, serum adiponectin levels were significantly raised, whereas glycated hemoglobin (HbA1c) levels were significantly lower compared with the baseline in the anagliptin group (P < 0.05), but not in the control group. This study showed that the DPP-4 inhibitor anagliptin reduces fasting apolipoprotein B-48 levels, suggesting that this drug may have beneficial effects on lipid metabolism possibly mediated by the inhibition of intestinal lipid transport
+output:
+Type 2 diabetes and dyslipidemia are diseases that collectively increase the risk of patients developing cardiovascular complications. Several incretin-based drugs are reported to improve lipid metabolism, and one of these medications, anagliptin, is a dipeptidyl peptidase-4 (DPP-4) inhibitor that has been shown to decrease serum triglyceride and low-density lipoproteins cholesterol. This multicenter, open-label, randomized (1:1), parallel group study was designed to evaluate the effects of anagliptin on serum lipid profiles (triglycerides, lipoproteins, apolipoproteins, and cholesterol fractions). 
+Here is the abstract:{content}
+    Please directly respond without any redundant words."""
+        messages=[{"role":"user","content":filter_prompt}]
+        filtered_content=chatgpt(messages)
+        cot_prompt=f"""I will provide the abstract of a research paper. Based on this abstract, Extract key information related to diabete research from the abstract in order.  
+##example:
+abstract:
+Severe hypoglycaemia (SH) is one of the most feared complications of type 1 diabetes (T1DM) with a reported prevalence of nearly 40%. In randomized trials of Multiple Daily Injections (MDI) and Continuous Subcutaneous Insulin Infusion (CSII) therapy there is a possible benefit of CSII in reducing SH. However few trials have used basal insulin analogues as the basal insulin in the MDI group and individuals with established SH have often been excluded from prospective studies. In published studies investigating the effect of Real Time Continuous Glucose Monitoring (RT-CGM) benefit in terms of reduced SH has not yet been demonstrated. The primary objective of this study is to elucidate whether in people with T1DM complicated by impaired awareness of hypoglycaemia (IAH), rigorous prevention of biochemical hypoglycaemia using optimized existing self-management technology and educational support will restore awareness and reduce risk of recurrent SH.
+output:
+Severe hypoglycaemia is one of the complications for type 1 diabetes; 
+Diabetes treatments include Multiple Daily Injections, Continuous Subcutaneous Insulin Infusion, Continuous Glucose Monitoring, and so on; 
+Among the treatments, Multiple Daily Injections and Continuous Subcutaneous Insulin Infusion are proven to help reduce the complication, severe hypoglycaemia.
+Here is the abstract:{filtered_content}"""
+        messages=[{"role":"user","content":cot_prompt}]
+        cot=chatgpt(messages)
+        cot=cot.replace("\n\n","\n")
+        articles[i]["cot"]=cot
+        file.write(json.dumps(articles[i]))
+        file.write('\n')
+        file.flush()  # 确保每次写入后立即保存
+
+print(count)
